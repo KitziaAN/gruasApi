9f44e09bebf5e90dd6d4ece24fe02ae1f15b4c5f
KitziaAN <utp0139196@alumno.utpuebla.edu.mx>
2023-11-06 07:45:11 -0600

    API para usuarios

---
+++ application/controllers/Api/Usuarios.php
@@ -0,0 +1,41 @@
+<?php
+
+require APPPATH . 'libraries/REST_Controller.php';
+
+class Usuarios extends REST_Controller{
+
+    public function __construct(){
+        parent:: __construct();
+        $this->load->database();
+    }
+
+    public function index_get($id=0){
+        
+        if (!empty($id)) {
+            $data = $this->db->get_where("usuarios", ['usuarioID'=>$id])->row_array();
+        }
+      
+        else{
+            $data = $this->db->get("usuarios")->result();
+        }
+        $this->response($data, REST_Controller::HTTP_OK);
+    }
+
+    public function index_post(){
+        $input = $this->input->post();
+        $this->db->insert("usuarios", $input);
+        $this->response(['Usuario agregado'], REST_Controller::HTTP_OK);
+    }
+
+    public function index_put($id){
+        $input = $this->put();
+        $this->db->update("usuarios", $input, array("usuarioID" => $id));
+        $this->response(['Usuario actualizado'], REST_Controller::HTTP_OK);
+    }
+
+    public function index_delete($id){
+        $this->db->delete("usuarios", array("usuarios" => $id));
+        $this->response(['Usuario eliminado'], REST_Controller::HTTP_OK);
+    }
+
+}
